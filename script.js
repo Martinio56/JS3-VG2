3d397e06c9e942181898d21b63a1331cf5f9a6f2
Nio <72229694+Martinio56@users.noreply.github.com>
2023-08-30 11:07:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+// Oppgave 1---------------------------------------------------------------------------------------------------------------------
+
+// var T1 = Number(prompt('Skriv et tall'))
+
+// if (T1 > 0) {
+//     console.log('Tallet er positivt');
+// } else {
+//     console.log('Tallet er negativt');
+// }
+
+// Oppgave 2---------------------------------------------------------------------------------------------------------------------
+
+// var T1 = Number(prompt('Skriv et tall'))
+// var T2 = Number(prompt('Skriv et tall til'))
+
+// if (T1 > T2) {
+//     console.log('T1 er større enn T2');
+// } else if (T1 < T2) {
+//     console.log('T1 er mindre enn T2');
+// } else {
+//     console.log('T1 er lik T2');
+// }
+
+// Oppgave 3---------------------------------------------------------------------------------------------------------------------
+
+// var T1 = Number(prompt('Skriv et tall fra 1-4'))
+
+// switch (T1) {
+//     case 1:
+//         console.log('Vår er første årstid');
+//         break;
+
+//     case 2:
+//         console.log('Sommer er andre årstid');
+//         break;
+    
+//     case 3:
+//         console.log('Høst er tredje årstid');
+//         break;
+        
+//     case 4:
+//         console.log('Vinter er fjerde årstid');
+//         break;
+
+//     default:
+//         console.log('Du skrev ikke et tall mellom 1-4');
+//         break;
+// }
+
+// Oppgave 4---------------------------------------------------------------------------------------------------------------------
+
+// for (let i = 0; i < 6; i++) {
+//     var T1 = Math.floor(Math.random() * 101);
+//     console.log(T1); 
+// }
+
+// Oppgave 5---------------------------------------------------------------------------------------------------------------------
+
+// for (let i = 1; i < 31; i++) {
+//     console.log(i);
+// }
+
+// Oppgave 6---------------------------------------------------------------------------------------------------------------------
+
+// for (let i = 1; i < 31; i++) {
+//     if (i % 2) {
+//         console.log(i);
+//     }
+// }
+
+// Oppgave 7---------------------------------------------------------------------------------------------------------------------
+
+// var T1 = Math.floor(Math.random() * 51);
+// var T2 = Number(prompt('Gjett tallet. Tallet er i mellom 1-50'));
+// console.log(T1);
+
+// while (true) {
+//     if (T2 < T1) {
+//         T2= Number(prompt('Tallet er høyere. Prøv igjen'));
+//     } else if (T2 > T1) {
+//         T2= Number(prompt('Tallet er lavere. Prøv igjen'));
+//     } else if (T2 == T1) {
+//         alert('Gratulerer Tallet er riktig');
+//         break;
+//     } 
+// }
+
+// ------------------------------------------------------------------------------------------------------------------------------
